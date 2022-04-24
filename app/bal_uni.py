d74729d4a33b54339b78be495196a97e528d92b5
林柏維 <r05521501@ntu.edu.tw>
2022-04-23 21:00:58 -0400

    add tool

--- app/bal_uni.py
+++ app/bal_uni.py
@@ -1,9 +1,12 @@
+from lib2to3.pgen2 import token
 import sys
 import asyncio
+from web3 import Web3
 from web3constant.Fantom.Url import FTM_RPC
 
 sys.path.append(".")
-from tools import util
+from tools import beethovenX, util
+from pool.base_pool import BasePool
 
 
 async def main():
@@ -13,7 +16,16 @@ async def main():
     else:
         print("Connection Fail")
         return
+
+    beethoven_pools_j = util.load_json("data/beethovenX_pool.json")
+    beethoven_vault_contract = beethovenX.creat_vault_contract(w3_async)
+    beets_pool_dict = await beethovenX.get_pool_dict_async(
+        w3_async, beethoven_pools_j, beethoven_vault_contract
+    )
     
+    for pool_addr, pool_obj in beets_pool_dict.items():
+        print(pool_obj.tokens)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
---
+++ tools/beethovenX.py
@@ -0,0 +1,66 @@
+import asyncio
+from web3 import Web3
+from web3constant.Fantom.Dex import BEETHOVEN_VAULT_ADDRESS
+from web3constant.abi.BalancerV2 import (
+    BEETHOVENX_VAULT_ABI,
+    BEETHOVENX_WEIGHTED_POOL_2TOKENS_ABI,
+    BEETHOVENX_WEIGHTED_POOL_ABI,
+)
+from .util import create_contract
+import sys
+
+sys.path.append("..")  # Adds higher directory to python modules path.
+from pool.base_pool import BasePool
+
+
+def creat_vault_contract(w3):
+    return create_contract(w3, BEETHOVEN_VAULT_ADDRESS, BEETHOVENX_VAULT_ABI)
+
+
+async def get_pool(beethoven_vault_contract, pool_id):
+    (pool_addr, pool_type), (tokens, balances, lastChangeBlock,) = await asyncio.gather(
+        beethoven_vault_contract.functions.getPool(pool_id).call(),
+        beethoven_vault_contract.functions.getPoolTokens(pool_id).call(),
+    )
+
+    return pool_addr, pool_type, tokens, balances, lastChangeBlock
+
+
+async def get_pool_weight_fee(w3_async, beethoven_vault_contract, pool_id):
+
+    pool_addr, pool_type, tokens, balances, lastChangeBlock = await get_pool(
+        beethoven_vault_contract, pool_id
+    )
+
+    pool_abi = BEETHOVENX_WEIGHTED_POOL_2TOKENS_ABI
+    if pool_type == 1:
+        pool_abi = BEETHOVENX_WEIGHTED_POOL_ABI
+    weighted_pool_contract = w3_async.eth.contract(address=pool_addr, abi=pool_abi)
+    weights, swapFeePercentage = await asyncio.gather(
+        weighted_pool_contract.functions.getNormalizedWeights().call(),
+        weighted_pool_contract.functions.getSwapFeePercentage().call(),
+    )
+    return pool_addr, pool_type, tokens, balances, weights, swapFeePercentage
+
+
+async def get_pool_dict_async(w3_async, beethoven_pools_j, beethoven_vault_contract):
+    pool_addr_dict = {}
+
+    tasks0 = []
+
+    for pool_id in beethoven_pools_j:
+        # GENERAL, MINIMAL_SWAP_INFO, TWO_TOKEN }
+        tasks0.append(
+            asyncio.create_task(
+                get_pool_weight_fee(w3_async, beethoven_vault_contract, pool_id)
+            )
+        )
+
+    for task in tasks0:
+
+        pool_addr, pool_type, tokens, balances, weights, swapFeePercentage = await task
+
+        fee = 1 - float(Web3.fromWei(swapFeePercentage, "ether"))
+        pool_obj = BasePool(pool_addr, tokens, weights, fee)
+        pool_addr_dict[pool_addr] = pool_obj
+    return pool_addr_dict
