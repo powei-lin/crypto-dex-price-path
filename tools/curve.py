a446276673e1b620520fc5564e1668b86f436b93
林柏維 <r05521501@ntu.edu.tw>
2022-05-02 17:08:45 -0400

    add curve

---
+++ tools/curve.py
@@ -0,0 +1,22 @@
+import asyncio
+from web3abi.Curve import (
+    CURVE_TRICRYPTO_ABI,
+    CURVE_TRICRYPTO_MATH_ABI,
+    CURVE_TRICRYPTO_VIEW_ABI,
+)
+from .util import create_contract
+
+
+def creat_tricrypto_contract(w3):
+    curve_3_coin_address = "0x3a1659Ddcf2339Be3aeA159cA010979FB49155FF"
+    return create_contract(w3, curve_3_coin_address, CURVE_TRICRYPTO_ABI)
+
+
+def create_tricrypto_math_contract(w3):
+    curve_math_address = "0x939986418baFb4E2d82A76E320767Ff02d250203"
+    return create_contract(w3, curve_math_address, CURVE_TRICRYPTO_MATH_ABI)
+
+
+def create_tricrypto_view_contract(w3):
+    curve_view_address = "0x4643A6600eae4851677A1f16d5e40Ef868c71717"
+    return create_contract(w3, curve_view_address, CURVE_TRICRYPTO_VIEW_ABI)
